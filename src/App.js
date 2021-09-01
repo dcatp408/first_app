e0930697f97b7136962138861162d755946ba4b8
Duy Pham <Duy@Users-MacBook-Pro-2.local>
2021-09-01 16:16:43 -0700

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      < >
+        <h1 className="h1">Hello Dojo</h1>
+        <h3>Things I need to do</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mount Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
